abb96127097a811fde6ba7a25a3ad8a8239ba1b0
Kelvin Jayaram <kelvin.rock.aplus@gmail.com>
2020-06-08 16:43:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,8 +14,8 @@ if (age >= 13 && age <= 19){
     return "You are a teenager!";}
   else if (age <= 12) {
     return "You are a kid";}
-  elseif (age > 19){
-    return "You are a grownup";}
+  else if (age > 19) 
+  return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
